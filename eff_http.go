16eeaded6c4244872ccc53842d8c943869b89dbe
谭奎 <1312308948@qq.com>
2021-09-07 13:37:25 +0800

    http并发请求

---
+++ eff_http.go
@@ -0,0 +1,39 @@
+package request
+
+import (
+	"log"
+	"sync"
+)
+
+type Resp struct {
+	Index int
+	Core  *Core
+	Err   error
+}
+
+// 并发请求
+func MultRequest(requests ...*Core) []Resp {
+	var wg sync.WaitGroup
+	wg.Add(len(requests))
+	ch := make(chan Resp, len(requests))
+	for i, req := range requests {
+		go func(i int, req *Core, ch chan Resp) {
+			defer wg.Done()
+			_, err := req.Send()
+			ch <- Resp{
+				Index: i,
+				Core:  req,
+				Err:   err,
+			}
+		}(i, req, ch)
+	}
+	wg.Wait()
+	close(ch)
+	// 将返回转化为数组
+	resps := make([]Resp, len(requests))
+	for one := range ch {
+		log.Println(one.Index)
+		resps[one.Index] = one
+	}
+	return resps
+}
